6d252f41af235c352f9d903e12661e7b1374a0ff
Dan Blais <blai0327@algonquinlive.com>
2024-07-11 18:11:50 -0400

    Second Commit: Began work on landing page Structure, added navigation buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,18 +37,115 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+  void initState() {
+    super.initState();
+  }
+
+  void customersPage() {}
+
+  void airplanesPage() {}
+
+  void flightsPage() {}
+
+  void reservationsPage() {}
+
+  Widget controlPanel() {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: 25, horizontal: 25),
+          child: Text(
+            "Poorman's Airlines Flight Management",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 35,
+                fontWeight: FontWeight.bold),
+          ),
         ),
+        Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: 25, horizontal: 50),
+            child: ElevatedButton(
+                onPressed: customersPage,
+                child: Text("Customer Management"))),
+        Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: 25, horizontal: 50),
+            child: ElevatedButton(
+                onPressed: airplanesPage,
+                child: Text("Airplane Management"))),
+        Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: 25, horizontal: 50),
+            child: ElevatedButton(
+                onPressed: flightsPage,
+                child: Text("Flight Management"))),
+        Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: 25, horizontal: 50),
+            child: ElevatedButton(
+                onPressed: reservationsPage,
+                child: Text("Reservation Management"))),
+      ],
+    );
+  }
+
+  Widget flightList() {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 400,
+          height: size.height - 200,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white70),
+          child:
+          Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child:
+                Text("Scheduled Flight List",
+                    style:
+                    TextStyle(
+                        fontSize: 20,
+                        fontWeight:
+                        FontWeight.bold)),),
+              Table()],
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(),
+          Align(
+            child: Container(
+              width: size.width * 0.8,
+              height: size.height * 0.8,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blueAccent),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: controlPanel()),
+                  Expanded(
+                      child: flightList()),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
