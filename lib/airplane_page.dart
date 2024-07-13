9f0828072e43f5363e11eec631eb3af2ea945a7c
Dan Blais <blai0327@algonquinlive.com>
2024-07-13 16:05:27 -0400

    Sixth Commit Airplane Branch: Finished implementing ListView.

--- lib/airplane_page.dart
+++ lib/airplane_page.dart
@@ -1,6 +1,8 @@
 import 'package:cst2335_group_project/utils.dart';
 import 'package:cst2335_group_project/validation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'airplane.dart';
 import 'airplane_database.dart';
@@ -28,6 +30,7 @@ class _AirplanePageState extends State<AirplanePage> {
   late final airplaneDAO;
   late bool noValidationErrors;
   late bool rowSelected;
+  late int selectedRow;
   late Airplane selectedAirplane;
 
   @override
@@ -45,44 +48,71 @@ class _AirplanePageState extends State<AirplanePage> {
     airplanes = [];
     noValidationErrors = true;
     rowSelected = false;
+    selectedRow = -1;
     initDatabase();
   }
 
   void initDatabase() async {
     db = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
-    airplaneDAO = db.listDao;
+    airplaneDAO = db.airplaneDao;
     List<Airplane> result = await airplaneDAO.findAllListItems();
     setState(() {
       airplanes.addAll(result);
     });
   }
 
-  void insertAirplane() {
-    if(!validation.validateType(insertTypeCont.value.text)) {
-      noValidationErrors = false;
-      createErrorSnackBar("Error: Airplane Type cannot be empty.");
-    }
-    if(!validation.validatePassengers(insertPassengerCont.value.text)) {
-      noValidationErrors = false;
-      createErrorSnackBar("Error: Max. Passengers cannot be empty, and must be an integer.");
-    }
-    if(!validation.validateSpeed(insertSpeedCont.value.text)) {
-      noValidationErrors = false;
-      createErrorSnackBar("Error: Max. Speed cannot be empty, and must be numeric.");
-    }
-    if(!validation.validateRange(insertRangeCont.value.text)) {
-      noValidationErrors = false;
-      createErrorSnackBar("Error: Max. Range cannot be empty, and must be numeric.");
-    }
-    if(noValidationErrors) {
-      Airplane airplane = Airplane(Utils.id++, insertTypeCont.value.text, int.parse(insertPassengerCont.value.text), double.parse(insertSpeedCont.value.text), double.parse(insertRangeCont.value.text));
-      airplaneDAO.insertList(airplane);
-      airplanes.add(airplane);
-      insertTypeCont.text = "";
-      insertPassengerCont.text = "";
-      insertSpeedCont.text = "";
-      insertRangeCont.text = "";
-    }
+  insertAirplane() {
+    setState(() {
+      if (!validation.validateType(insertTypeCont.value.text)) {
+        noValidationErrors = false;
+        createErrorSnackBar("Error: Airplane Type cannot be empty.");
+      }
+      if (!validation.validatePassengers(insertPassengerCont.value.text)) {
+        noValidationErrors = false;
+        createErrorSnackBar(
+            "Error: Max. Passengers cannot be empty, and must be an integer.");
+      }
+      if (!validation.validateSpeed(insertSpeedCont.value.text)) {
+        noValidationErrors = false;
+        createErrorSnackBar(
+            "Error: Max. Speed cannot be empty, and must be numeric.");
+      }
+      if (!validation.validateRange(insertRangeCont.value.text)) {
+        noValidationErrors = false;
+        createErrorSnackBar(
+            "Error: Max. Range cannot be empty, and must be numeric.");
+      }
+      if (noValidationErrors) {
+        Airplane airplane = Airplane(
+            Utils.id++,
+            insertTypeCont.value.text,
+            int.parse(insertPassengerCont.value.text),
+            double.parse(insertSpeedCont.value.text),
+            double.parse(insertRangeCont.value.text));
+        airplaneDAO.insertList(airplane);
+        airplanes.add(airplane);
+        insertTypeCont.text = "";
+        insertPassengerCont.text = "";
+        insertSpeedCont.text = "";
+        insertRangeCont.text = "";
+      }
+
+      noValidationErrors = true;
+    });
+  }
+
+  updateAirplane() {
+    setState(() {
+      selectedRow = -1;
+      rowSelected = false;
+    });
+  }
+
+  deleteAirplane() {
+    setState(() {
+      selectedRow = -1;
+      rowSelected = false;
+    });
   }
 
   Widget createErrorSnackBar(String errorMsg) {
@@ -121,7 +151,9 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar,
               height: 20,
-              child: TextField(controller: insertTypeCont),
+              child: TextField(
+                  controller: insertTypeCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -142,7 +174,9 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar - 23,
               height: 20,
-              child: TextField(controller: insertPassengerCont),
+              child: TextField(
+                  controller: insertPassengerCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -163,7 +197,9 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar + 14,
               height: 20,
-              child: TextField(controller: insertSpeedCont),
+              child: TextField(
+                  controller: insertSpeedCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -184,16 +220,16 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar + 14,
               height: 20,
-              child: TextField(controller: insertRangeCont),
+              child: TextField(
+                  controller: insertRangeCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 5),
           child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
+            onPressed: insertAirplane,
             style: ButtonStyle(
                 backgroundColor:
                     MaterialStateColor.resolveWith((states) => Colors.black38),
@@ -221,41 +257,68 @@ class _AirplanePageState extends State<AirplanePage> {
   }
 
   Widget listView(double width, double height) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: width,
-          height: height,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20), color: Colors.black54),
-          child: Column(
-            children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      "Airplane Inventory",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: "Satoshi",
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                width: width - 20,
-                height: height - 45,
-              ),
-            ],
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20), color: Colors.black54),
+      child: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: Text(
+              "Airplane Inventory",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Satoshi",
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: SizedBox(
+              width: width * 0.9,
+              height: height * 0.95,
+              child: ListView.builder(
+                  itemBuilder: (item, rowNum) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedRow = rowNum;
+                          selectedAirplane = airplanes[rowNum];
+                          rowSelected = true;
+                        });
+                      },
+                      child: DecoratedBox(
+                          decoration: BoxDecoration(
+                              color: selectedRow == rowNum
+                                  ? Colors.blueAccent
+                                  : Colors.black87),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                    width: 50,
+                                    child: Text(
+                                      "${rowNum + 1}.",
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    )),
+                                Text(
+                                  airplanes[rowNum].airplaneType,
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ])),
+                    );
+                  },
+                  itemCount: airplanes.length),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -270,7 +333,7 @@ class _AirplanePageState extends State<AirplanePage> {
             style: TextStyle(
                 color: Colors.white,
                 fontFamily: "Satoshi",
-                fontSize: 20,
+                fontSize: 35,
                 fontWeight: FontWeight.bold),
           ),
         ),
@@ -291,7 +354,9 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar,
               height: 20,
-              child: TextField(controller: detailsTypeCont),
+              child: TextField(
+                  controller: detailsTypeCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -312,7 +377,9 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar - 23,
               height: 20,
-              child: TextField(controller: detailsPassengerCont),
+              child: TextField(
+                  controller: detailsPassengerCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -333,7 +400,9 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar + 14,
               height: 20,
-              child: TextField(controller: detailsSpeedCont),
+              child: TextField(
+                  controller: detailsSpeedCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -354,23 +423,23 @@ class _AirplanePageState extends State<AirplanePage> {
             SizedBox(
               width: textFieldScalar + 14,
               height: 20,
-              child: TextField(controller: detailsRangeCont),
+              child: TextField(
+                  controller: detailsRangeCont,
+                  style: const TextStyle(color: Colors.white)),
             ),
           ],
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 9),
+          padding: const EdgeInsets.symmetric(vertical: 5),
           child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
+            onPressed: updateAirplane,
             style: ButtonStyle(
                 backgroundColor:
-                MaterialStateColor.resolveWith((states) => Colors.black38),
+                    MaterialStateColor.resolveWith((states) => Colors.black38),
                 foregroundColor:
-                MaterialStateColor.resolveWith((states) => Colors.black38),
+                    MaterialStateColor.resolveWith((states) => Colors.black38),
                 overlayColor:
-                MaterialStateColor.resolveWith((states) => Colors.black12)),
+                    MaterialStateColor.resolveWith((states) => Colors.black12)),
             child: const SizedBox(
                 width: 150,
                 height: 30,
@@ -379,43 +448,71 @@ class _AirplanePageState extends State<AirplanePage> {
                     Icons.add,
                     color: Colors.greenAccent,
                   ),
-                  Padding(padding: EdgeInsets.symmetric(horizontal: 7)),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
                   Text("Update Airplane",
                       style:
-                      TextStyle(color: Colors.white, fontFamily: "Satoshi"))
+                          TextStyle(color: Colors.white, fontFamily: "Satoshi"))
                 ])),
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 9),
+          padding: const EdgeInsets.symmetric(vertical: 7),
           child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
+            onPressed: deleteAirplane,
             style: ButtonStyle(
                 backgroundColor:
-                MaterialStateColor.resolveWith((states) => Colors.black38),
+                    MaterialStateColor.resolveWith((states) => Colors.black38),
                 foregroundColor:
-                MaterialStateColor.resolveWith((states) => Colors.black38),
+                    MaterialStateColor.resolveWith((states) => Colors.black38),
                 overlayColor:
-                MaterialStateColor.resolveWith((states) => Colors.black12)),
+                    MaterialStateColor.resolveWith((states) => Colors.black12)),
             child: const SizedBox(
                 width: 150,
                 height: 30,
                 child: Row(children: [
                   Icon(
-                    Icons.add,
+                    Icons.delete,
                     color: Colors.greenAccent,
                   ),
                   Padding(padding: EdgeInsets.symmetric(horizontal: 7)),
                   Text("Delete Airplane",
                       style:
-                      TextStyle(color: Colors.white, fontFamily: "Satoshi"))
+                          TextStyle(color: Colors.white, fontFamily: "Satoshi"))
                 ])),
           ),
         ),
       ],
     );
+  }
+
+  Widget portraitWidget(Size size) {
+    Widget widget;
+    if (!rowSelected) {
+      widget = controlPanel(size, 200);
+      return widget;
+    } else {
+      widget = details(size, 200);
+      detailsTypeCont.text = selectedAirplane.airplaneType;
+      detailsPassengerCont.text = selectedAirplane.maxPassengers.toString();
+      detailsSpeedCont.text = selectedAirplane.maxSpeed.toString();
+      detailsRangeCont.text = selectedAirplane.maxRange.toString();
+      return widget;
+    }
+  }
+
+  Widget landscapeWidget(Size size) {
+    Widget widget;
+    if (!rowSelected) {
+      widget = controlPanel(size, 170);
+      return widget;
+    } else {
+      widget = details(size, 170);
+      detailsTypeCont.text = selectedAirplane.airplaneType;
+      detailsPassengerCont.text = selectedAirplane.maxPassengers.toString();
+      detailsSpeedCont.text = selectedAirplane.maxSpeed.toString();
+      detailsRangeCont.text = selectedAirplane.maxRange.toString();
+      return widget;
+    }
   }
 
   @override
@@ -427,23 +524,24 @@ class _AirplanePageState extends State<AirplanePage> {
           children: [
             Align(
               child: Container(
-                width: size.width,
-                height: size.height,
-                decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        colorFilter: ColorFilter.mode(
-                            Colors.black.withOpacity(0.2), BlendMode.dstATop),
-                        image:
-                            const AssetImage("assets/images/plane-wing.png"))),
-                child: Column(
-                  children: [
-                    Expanded(child: controlPanel(size, 200)),
-                    Padding(padding: const EdgeInsets.only(bottom: 10), child: listView(size.width - 50, size.height / 1.8))
-                  ],
-                ),
-              ),
+                  width: size.width,
+                  height: size.height,
+                  decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                          image: const AssetImage(
+                              "assets/images/plane-wing.png"))),
+                  child: Column(
+                    children: [
+                      Expanded(child: portraitWidget(size)),
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: listView(size.width - 50, size.height / 2))
+                    ],
+                  )),
             )
           ],
         );
@@ -452,24 +550,26 @@ class _AirplanePageState extends State<AirplanePage> {
           children: [
             Align(
               child: Container(
-                width: size.width,
-                height: size.height,
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.2), BlendMode.dstATop),
-                      image: const AssetImage("assets/images/plane-wing.png")),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(child: controlPanel(size, 100)),
-                    Expanded(child: listView((size.width / 2), size.height - 55)),
-                    Expanded(child: details(size, 100))
-                  ],
-                ),
-              ),
+                  width: size.width,
+                  height: size.height,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                        image:
+                            const AssetImage("assets/images/plane-wing.png")),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(child: landscapeWidget(size)),
+                      Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child:
+                              listView((size.width / 2) - 35, size.height - 50))
+                    ],
+                  )),
             )
           ],
         );
