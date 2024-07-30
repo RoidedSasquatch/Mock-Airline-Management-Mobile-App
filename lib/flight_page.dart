06085e14646c8d959e898c4a9ea35ef683b29f9b
Michael <michael.dagher03@gmail.com>
2024-07-30 15:09:41 -0400

    Commit 4: Page design started.

--- lib/flight_page.dart
+++ lib/flight_page.dart
@@ -1,3 +1,6 @@
+import 'package:cst2335_group_project/AppLocalizations.dart';
+import 'package:cst2335_group_project/flight.dart';
+import 'package:cst2335_group_project/flight_dao.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,9 +16,32 @@ class FlightPage extends StatefulWidget {
 
 class FlightPageState extends State<FlightPage> {
 
+  List<Flight> flightList = [];
+
+  late FlightDao flightDao;
+
+  Flight? selectedFlight;
+
+  late TextEditingController _originController;
+  late TextEditingController _destinationController;
+  late TextEditingController _departureController;
+  late TextEditingController _arrivalController;
+
+  List<Locale> locales = [Locale("en", "CA"), Locale("ja")];
+  var currentLocIndex = 0;
+
+  String translate(String key) {
+    return AppLocalizations.of(context)?.translate(key)??"No Translation Available";
+  }
+
   @override
   void initState() {
     super.initState();
+
+    _originController = TextEditingController();
+    _destinationController = TextEditingController();
+    _departureController = TextEditingController();
+    _arrivalController = TextEditingController();
   }
 
   @override
@@ -23,12 +49,83 @@ class FlightPageState extends State<FlightPage> {
     super.dispose();
   }
 
+
+
+  Widget flightControl() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              translate("FL_origin")
+            ),
+            SizedBox(
+              width: 100,
+              height: 20,
+              child: TextField(
+                  controller: _originController,
+                  style: const TextStyle(color: Colors.white)
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Text(
+                translate("FL_destination")
+            ),
+            SizedBox(
+              width: 100,
+              height: 20,
+              child: TextField(
+                  controller: _destinationController,
+                  style: const TextStyle(color: Colors.white)
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Text(
+                translate("FL_departure")
+            ),
+            SizedBox(
+              width: 100,
+              height: 20,
+              child: TextField(
+                  controller: _departureController,
+                  style: const TextStyle(color: Colors.white)
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Text(
+                translate("FL_arrival")
+            ),
+            SizedBox(
+              width: 100,
+              height: 20,
+              child: TextField(
+                  controller: _arrivalController,
+                  style: const TextStyle(color: Colors.white)
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "TEMP APP BAR TITLE",
+          translate("FL_flight_operations"),
           style: const TextStyle(
             color: Colors.white,
             fontFamily: "Satoshi",
@@ -37,6 +134,13 @@ class FlightPageState extends State<FlightPage> {
           )
         ),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            flightControl()
+          ],
+        )
+      )
     );
   }
 }
